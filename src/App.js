af4397537baaeaa1254901d34a4359cf2555ab21
Joanna <askat58@gmail.com>
2021-04-08 21:02:43 +0100

    Start to create the search engine
    - add new component Dictionary.js and style Dictionary.css
    - add search input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import logo from "./logo.png";
 import "./App.css";
 
+import Dictionary from "./Dictionary";
+
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            Coded by{" "}
+            <a
+              href="https://www.linkedin.com/in/joanna-el%C5%BCbieta-t-288561181/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Joanna Tucka
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
