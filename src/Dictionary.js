7d3cac5fcd572ec00b48ce3d025b679d523bdfc1
Joanna <askat58@gmail.com>
2021-04-11 13:22:48 +0100

    Add components
    - create new state
    - add new components
    - add searcg content

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import Results from "./Results";
 import axios from "axios";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState(null);
+  const [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    //console.log(response.data[0].meanings[0].definitions[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    
+
     // documentation: https://api.dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
@@ -26,6 +29,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
