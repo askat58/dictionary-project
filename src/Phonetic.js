585b85bb6be243782cff0704ce3265582b66ccf2
Joanna <askat58@gmail.com>
2021-04-12 12:18:31 +0100

    Add phonetics of the words

---
+++ src/Phonetic.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  return (
+    <div className="Phonetic">
+      <a href={props.phonetic.audio} target="blank">
+        Listen
+      </a>
+      <br />
+    </div>
+  );
+}
