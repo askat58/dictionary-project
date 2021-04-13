047001ca40cbd88098e0b37fa6924fca28d9fed8
Joanna <askat58@gmail.com>
2021-04-13 15:40:55 +0100

    Styling the application

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -37,8 +37,13 @@ export default function Dictionary(props) {
     return (
       <div className="Dictionary">
         <section>
+          <h1>What word do tou want to look up?</h1>
           <form onSubmit={handleSubmit}>
-            <input type="search" onChange={handleKeywordChange} />
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
           </form>
           <div className="hint">
             suggested words: sunset, wine, yoga, plant...
@@ -49,6 +54,6 @@ export default function Dictionary(props) {
     );
   } else {
     load();
-    return "Loading.."
+    return "Loading..";
   }
 }
